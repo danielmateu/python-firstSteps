cd22bcf4c20a4a55e85e6cb80f566ba10daa0bca
Daniel Mateu <danielmateu86@gmail.com>
2023-01-24 12:04:54 +0100

    Operadores

---
+++ operadores/op_comparacion.py
@@ -0,0 +1,23 @@
+#Operadores de comparacion
+
+es_igual_que = 5 == 6 # Devuelve truoo o false
+
+es_distinto_de = 5 != 6
+
+es_mayor_que = 5 > 6
+
+es_mayor_o_igual_que = 5 >= 6
+
+es_menor_que = 5 < 6
+
+es_menor_o_igual_que = 5 <= 6
+
+print(es_menor_o_igual_que, es_igual_que)
+
+#Calculos combinados
+
+a = 10
+b = 20
+c = a + b + 10
+comparacion = a + b <= c
+print(comparacion)
