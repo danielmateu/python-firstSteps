65b3218ec39579021b78e416a29247d8856bf279
Daniel Mateu <danielmateu86@gmail.com>
2023-01-24 15:03:08 +0100

    input

---
+++ inputs/input_num.py
@@ -0,0 +1,11 @@
+#Pedirle un nuemro al usuario
+
+numero = input('Dame un número: ');
+
+# Convierto el string a int y lo multiplico por 2
+resultado = int(numero) * 2
+
+# Convierto el string a float y lo multiplico por 2
+resultado_float = float(numero) / 2
+
+print(resultado_float)
---
+++ inputs/input_string.py
@@ -0,0 +1,4 @@
+# pedirle un dato al usuario
+
+nombre = input('Dame tu nombre: ') # Siempre nos devuelve un string
+print(f'Bienvenido {nombre}')
