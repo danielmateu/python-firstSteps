a5e70dc7715a03fd113b764c86b4c0eeddd91c3a
Daniel Mateu <danielmateu86@gmail.com>
2023-01-24 10:54:15 +0100

    definicion de variables, concatenación y eliminación de variables

--- variables/variables.py
+++ variables/variables.py
@@ -3,9 +3,22 @@
 a = 5
 b = 8
 c = a + b
+
 print(c)
 
 nombre = 'Dani'
 print(nombre)
+
+# Concatenacion
+bienvenida = "Hola " + nombre + " cómo estás?"
+print(bienvenida)
+
+# Tomamos un dato y se transforma en texto
+bienvenidaF = f"Hola {nombre} cómo estás?" 
+print(bienvenidaF)
+
+# Para eliminar la declaración de la variable, borrar datos
+del nombre;
+print(bienvenidaF)
 
 
