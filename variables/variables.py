e2e25df4fc5f5f0b2f435635a2128a8e94d3ddf0
Daniel Mateu <danielmateu86@gmail.com>
2023-01-24 11:17:20 +0100

    fin variables + datos compuestos

--- variables/variables.py
+++ variables/variables.py
@@ -1,24 +1,34 @@
-'Las variables son espacios de memoria que usamos dentro del programa'
+definicion = 'Las variables son espacios de memoria que usamos dentro del programa donde almacenamos datos'
 
+# Definendo la variable
 a = 5
 b = 8
 c = a + b
 
 print(c)
 
-nombre = 'Dani'
-print(nombre)
+#Definiendo variable con camelCase
+nombreCompleto = 'Daniel Mateu'
+print(nombreCompleto)
+
+#Definiendo variable con snake_case, como se recomienda en Python 🐍
+nombre_completo = 'Daniel Mateu'
+print(nombre_completo)
 
 # Concatenacion
-bienvenida = "Hola " + nombre + " cómo estás?"
+bienvenida = "Hola " + nombreCompleto + " cómo estás?"
 print(bienvenida)
 
-# Tomamos un dato y se transforma en texto
-bienvenidaF = f"Hola {nombre} cómo estás?" 
+# Tomamos un dato y se transforma en texto concatenando con f-strings
+bienvenidaF = f"Hola {nombreCompleto} cómo estás?" 
 print(bienvenidaF)
 
 # Para eliminar la declaración de la variable, borrar datos
-del nombre;
+del nombreCompleto;
 print(bienvenidaF)
+
+# Operaciones de pertenencia / identidad -> Devuelve true o false en función de si está o no
+print('Hola' in bienvenidaF) #true
+print('Hola' not in bienvenidaF) #false
 
 
