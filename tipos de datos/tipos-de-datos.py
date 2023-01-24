43eb2df22020c8298186963e1d9ae0273744ca43
Daniel Mateu <danielmateu86@gmail.com>
2023-01-24 10:08:08 +0100

    first commit

---
+++ tipos de datos/tipos-de-datos.py
@@ -0,0 +1,12 @@
+"string"
+'string'
+'''
+Esto es como los backticks
+'''
+
+4, 'Int'
+4.5, 'Float'
+'Boolean'
+
+False
+True
---
+++ variables/variables.py
@@ -0,0 +1,11 @@
+'Las variables son espacios de memoria que usamos dentro del programa'
+
+a = 5
+b = 8
+c = a + b
+print(c)
+
+nombre = 'Dani'
+print(nombre)
+
+
