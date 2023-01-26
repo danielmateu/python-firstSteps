fc6a4f90a7302a591bf16a1d19a2098c689eeb02
Daniel Mateu <danielmateu86@gmail.com>
2023-01-26 10:33:21 +0100

    lectura de csv

--- archivos/leer_csv_con_pandas.py
+++ archivos/leer_csv_con_pandas.py
@@ -19,43 +19,56 @@ cadena = '0123456789'
 
 #ORdenando el df por la edad de forma ascendente
 df_ordenado_ascendente = df.sort_values('edad')
-print(df_ordenado_ascendente)
+# print(df_ordenado_ascendente)
 
 #ORdenando el df por la edad de forma descendente
 df_ordenado_descendente = df.sort_values('edad', ascending=False)
-print(df_ordenado_descendente)
+# print(df_ordenado_descendente)
 
 #Concatenando los dos df
 df_concatenado = pd.concat([df, df2])
-print(df_concatenado)
+# print(df_concatenado)
 
 # Accediendo a las 2 primeras fila con head()
 primera_fila = df.head(2)
-print(primera_fila)
+# print(primera_fila)
 
-# Accediendo a las 2 primeras fila con tail()
+# Accediendo a las 2 últimas fila con tail()
 ultima_fila = df.tail(2)
-print(ultima_fila)
+# print(ultima_fila)
 
 # Accediendo a la cantidad de filas y columnas con shape
 filas_y_columnas_totales = df.shape
-print(filas_y_columnas_totales)
+# print(filas_y_columnas_totales)
 # filas_totales = filas_y_columnas_totales[0]
 # columnas_totales = filas_y_columnas_totales[1]
 filas_totales, columnas_totales = df.shape
 
-# Obteniendo data estadistica del df
+# Obteniendo data estadistica del df con describe()
 df_info = df.describe()
-print(df_info)
+# print(df_info)
 
-# Accediendo a un elemento especifico del df con loc
-elemento_especifico_loc = df.loc[3:'edad'];
-elemento_especifico_iloc = df.iloc[2,2];
-print(elemento_especifico_loc)
-print(elemento_especifico_iloc)
+# Accediendo a un elemento especifico del df con loc[] por el valor
+elemento_especifico_loc = df.loc[2:"edad"];
+# Accediendo a un elemento especifico del df con iloc[] por el indice
+elemento_especifico_iloc = df.iloc[0,2];
+# print(elemento_especifico_loc)
+# print(elemento_especifico_iloc)
 
-# Accediento a tdas las filas de una columna
-apellidos = df.loc[:'apellidos']
+# Accediento a todas las filas de una columna
+apellidos_loc = df.loc[:'apellidos']
+# print(apellidos_loc)
 apellidos_iloc = df.iloc[:,1]
-print(apellidos)
+# print(apellidos_iloc)
+
+# Accediendo a la fila 3 con loc
+fila_3_loc = df.loc[2,:]
+# print(fila_3_loc)
+# Accediendo a la fila 3 con iloc es la mísma condicion que con loc
+fila_3_iloc = df.iloc[2,:]
+
+# Accediendo a filas con edad > 30
+mayor_que_30 = df.loc[df['edad']>30,:]
+print(mayor_que_30)
+
 
